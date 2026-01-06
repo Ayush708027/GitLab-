1ed71bd03a79142abc221e8d6048dab205e19865
Ayush Prajapati <prajapatiayush426@gmail.com>
2026-01-06 11:31:10 +0530

    GitLab Concepts

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Ayush Prajapati");
+
+/**
+ * Adds two numbers and returns the sum.
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function add(a, b) {
+	return a + b;
+}
+
+// Example usage
+console.log("add(2, 3) =", add(2, 3));
